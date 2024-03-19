4b3c2a6ba0175e9944584b7d4883cf2053306134
Cosmic Process <126974221+cosmicproc@users.noreply.github.com>
2024-03-19 07:52:09 +0300

    Remove custom fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,19 +11,3 @@
 <div class="dark:bg-black dark:[&>*]:text-white min-h-screen flex flex-col">
 	<slot />
 </div>
-
-<style>
-	@font-face {
-		font-family: 'Source Sans';
-		src: url('../fonts/SourceSans3VF-Upright.woff2');
-		font-style: normal;
-		font-display: swap;
-	}
-
-	@font-face {
-		font-family: 'Source Sans';
-		src: url('../fonts/SourceSans3VF-Italic.woff2');
-		font-style: italic;
-		font-display: swap;
-	}
-</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-import defaultTheme from 'tailwindcss/defaultTheme';
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {},
-		fontFamily: {
-			...defaultTheme.fontFamily,
-			sans: ['"Source Sans"', ...defaultTheme.fontFamily.sans]
-		}
+		extend: {}
 	},
 	plugins: []
 };
